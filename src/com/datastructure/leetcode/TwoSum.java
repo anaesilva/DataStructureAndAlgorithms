70e0aa7ff33e50554dcfb1ab120ee4a2291ed99c
Ana Silva <aeusebio@uolinc.com>
2024-04-07 04:55:35 -0300

    Solution

--- src/com/datastructure/leetcode/TwoSum.java
+++ src/com/datastructure/leetcode/TwoSum.java
@@ -2,6 +2,7 @@ package com.datastructure.leetcode;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
@@ -10,16 +11,27 @@ public class TwoSum {
         int[] newArray = new int[2];
 
         for(int i = 0; i < nums.length; i++) {
-            hashMap.put(i, nums[i]);
-        } int dif = 0;
-        for (int i = 0; i < nums.length; i++) { dif = target - nums[i];
+            hashMap.put(nums[i], i);
+        }
+
+        System.out.println("key, value: " + ((HashMap<Integer, Integer>) hashMap));
+        System.out.println("Get value " + hashMap.get(2));
+        System.out.println(hashMap.containsKey(11));
+
+
+        int dif = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            dif = target - nums[i];
+
             if (hashMap.containsKey(dif) && dif + nums[i] == target) {
-                newArray = new int[]{};break;
+                newArray = new int[]{hashMap.get(dif), hashMap.get(nums[i])};
             }
         }
 
+
         for (int x : newArray) {
-            System.out.println(newArray[x]);
+            System.out.println("new: " + newArray[x]);
         }
         return newArray;
     }
